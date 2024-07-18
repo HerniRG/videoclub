b0202c3aee1821b53549d18f1218ab8a31f53028
HernánRG <hernanrg85@gmail.com>
2024-07-18 16:08:16 +0200

    fix: __eq__ of Class Director

--- app/modelos.py
+++ app/modelos.py
@@ -1,12 +1,23 @@
 from abc import ABC, abstractmethod
+import csv
 
-class Director():
+class Director:
     def __init__(self, nombre, id=-1) -> None:
         self.nombre = nombre
         self.id = id
 
+    def  __repr__(self) -> str:
+        return f"Director({self.id}): {self.nombre}"
+    
+    def __eq__(self, other: object) -> bool:
+        isEqual = False
+        if isinstance(other, Director):
+            if self.nombre == other.nombre and self.id == other.id:
+                isEqual = True        
+        return isEqual
 
 class DAO(ABC):
+    """
     @abstractmethod
     def guardar(self, instancia):
         pass
@@ -22,8 +33,22 @@ class DAO(ABC):
     @abstractmethod
     def consultar(self, id):
         pass
-    
+    """
     @abstractmethod
     def todos(self):
         pass
+
+class DAO_CSV_Director(DAO):
     
+    def __init__(self, path):
+        self.path = path
+
+    def todos(self):
+        with open(self.path, "r", newline="") as fichero:
+            lector_csv = csv.DictReader(fichero, delimiter=";", quotechar="'")
+            lista = []
+            for registro in lector_csv:
+                lista.append(Director(registro["nombre"], int(registro["id"])))
+        return lista
+
+
--- tests/test_modelos.py
+++ tests/test_modelos.py
@@ -1,7 +1,14 @@
-from app.modelos import Director
+from app.modelos import Director, DAO_CSV_Director
 
 def test_create_director():
     director = Director("Robert Redford", -1)
 
     assert director.nombre == "Robert Redford"
     assert director.id == -1
+
+def test_dao_directores_traer_todos():
+    dao = DAO_CSV_Director("tests/data/directores.csv")
+    directores = dao.todos()
+
+    assert len(directores) == 8
+    assert directores[7] == Director("Charlie Chaplin", 8)
