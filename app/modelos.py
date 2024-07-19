a696453735529c3e15575ba63b189080fe87c9ee
HernánRG <hernanrg85@gmail.com>
2024-07-19 20:23:18 +0200

    todos los test OK y creado Dao peliculas y directores con CRUD

--- app/modelos.py
+++ app/modelos.py
@@ -18,7 +18,6 @@ class Director:
     def __hash__(self) -> int:
         return hash((self.id, self.nombre))
 
-
 class Pelicula:
     def __init__(self, titulo, sinopsis, director: object, id = -1) -> None:
         self.titulo = titulo
@@ -41,6 +40,17 @@ class Pelicula:
         else:
             raise TypeError(f"{value} debe ser un entero o instancia de Director")
     
+    def  __repr__(self) -> str:
+        return f"Pelicula: Título ({self.titulo}), Sinopsis {self.sinopsis} y Director {self.director.nombre}."
+    
+    def __eq__(self, other: object) -> bool:
+        isEqual = False
+        if isinstance(other, self.__class__): # self.__class__ es lo mismo que Director
+            isEqual = self.id == other.id and self.director == other.director and self.sinopsis == other.sinopsis and self.titulo == other.titulo      
+        return isEqual
+    
+    def __hash__(self) -> int:
+        return hash((self.id, self.titulo, self.sinopsis, self.director))
 
 class DAO(ABC):
     
@@ -48,11 +58,10 @@ class DAO(ABC):
     def guardar(self, instancia):
         pass
     
-    """
     @abstractmethod
     def actualizar(self, instancia):
         pass
-    """
+    
 
     @abstractmethod
     def borrar(self, id):
@@ -102,4 +111,65 @@ class DAO_CSV_Director(DAO):
             for registro in lector_csv:
                 lista.append(Director(registro["nombre"], int(registro["id"])))
         return lista
+    
+    def actualizar(self, instancia):
+        directores = self.todos()
+        for i, director in enumerate(directores):
+            if director.id == instancia.id:
+                directores[i] = instancia # si coincide los id de director y el que metimos lo sustituye por el que metimos con los nuevos datos
+        with open(self.path, "w", newline="") as fichero:
+            escribir_csv = csv.DictWriter(fichero, delimiter=";", quotechar="'", fieldnames=['nombre', 'id'], lineterminator='\n')
+            escribir_csv.writeheader()
+            for director in directores:
+                escribir_csv.writerow({"nombre": director.nombre, "id": director.id})
 
+class DAO_CSV_Pelicula(DAO):
+    def __init__(self, path):
+        self.path = path 
+
+    def guardar(self, instancia):
+        with open(self.path, "a", newline="") as fichero:
+            escribir_csv = csv.DictWriter(fichero, delimiter=";", quotechar="'", fieldnames=["id", "titulo", "sinopsis", "director_id"], lineterminator='\n')
+            escribir_csv.writerow({"id": instancia.id, "titulo": instancia.titulo, "sinopsis": instancia.sinopsis, "director_id": instancia._director_id})
+
+    def todos(self):
+        with open(self.path, "r", newline="") as fichero:
+            lector_csv = csv.DictReader(fichero, delimiter=";", quotechar="'")
+            lista = []
+            for registro in lector_csv:
+                try:
+                    id = int(registro["id"])
+                    director_id = int(registro["director_id"])
+                    lista.append(Pelicula(registro["titulo"], registro["sinopsis"], director_id, id))
+                except ValueError as e:
+                    print(f"Error processing record {registro}: {e}")
+        return lista
+
+    def consultar(self, id):
+        peliculas = self.todos()
+        for pelicula in peliculas:
+            if pelicula.id == id:
+                return pelicula
+        return None
+    
+    def borrar(self, id):
+        pelicula = self.consultar(id)
+        peliculas = self.todos()
+        if pelicula in peliculas:
+            peliculas.remove(pelicula)
+        with open(self.path, "w", newline="") as fichero:
+            escribir_csv = csv.DictWriter(fichero, delimiter=";", quotechar="'", fieldnames=["id","titulo","sinopsis","director_id"], lineterminator='\n') # lineterminator en google
+            escribir_csv.writeheader() # lo he tenido que consultar porque me escirbía sin las etiquetas nombre e id
+            for pelicula in peliculas:
+                escribir_csv.writerow({"id": pelicula.id, "titulo": pelicula.titulo, "sinopsis": pelicula.sinopsis, "director_id": pelicula._director_id})
+    
+    def actualizar(self, instancia):
+        peliculas = self.todos()
+        for i, pelicula in enumerate(peliculas):
+            if pelicula.id == instancia.id:
+                peliculas[i] = instancia # si coincide los id de pelicula y el que metimos lo sustituye por el que metimos con los nuevos datos
+        with open(self.path, "w", newline="") as fichero:
+            escribir_csv = csv.DictWriter(fichero, delimiter=";", quotechar="'", fieldnames=["id", "titulo", "sinopsis", "director_id"], lineterminator='\n')
+            escribir_csv.writeheader()
+            for pelicula in peliculas:
+                escribir_csv.writerow({"id": pelicula.id, "titulo": pelicula.titulo, "sinopsis": pelicula.sinopsis, "director_id": pelicula._director_id})
--- tests/test_modelos.py
+++ tests/test_modelos.py
@@ -1,4 +1,4 @@
-from app.modelos import Director, Pelicula, DAO_CSV_Director
+from app.modelos import Director, Pelicula, DAO_CSV_Director, DAO_CSV_Pelicula
 
 def test_create_director():
     director = Director("Robert Redford", -1)
@@ -26,7 +26,6 @@ def test_dao_directores_guardar_director():
 
 def test_dao_directores_consulta():
     dao = DAO_CSV_Director("tests/data/directores.csv")
-    directores = dao.todos()
     alfred = dao.consultar(3)
 
     assert alfred == Director("Alfred Hitchcock", 3)
@@ -42,8 +41,20 @@ def test_dao_directores_borrar_director():
 
     dao.guardar(Director("Charlie Chaplin", 8))
 
-def test_create_pelicula():
+def test_dao_directores_actualizar_director():
+    dao = DAO_CSV_Director("tests/data/directores.csv")
+    
+    director = dao.consultar(1)
+    director.nombre = "Wolframio Actualizado"
+    dao.actualizar(director)
+    
+    director_actualizado = dao.consultar(1)
+    assert director_actualizado.nombre == "Wolframio Actualizado"
 
+    director.nombre = "Director 1"  # Restaurar valor original
+    dao.actualizar(director)
+
+def test_create_pelicula():
     pelicula = Pelicula("El señor de los anillos", "Sauron es muy malo", 9)
 
     assert pelicula.titulo == "El señor de los anillos"
@@ -63,14 +74,70 @@ def test_create_pelicula_and_informar_director_completo():
     assert pelicula.director is director
 
 def test_asigna_director_a_pelicula():
-        pelicula = Pelicula("El señor de los anillos", "Sauron es muy malo", -1)
-        director = Director("Peter Jackson", 9)
+    pelicula = Pelicula("El señor de los anillos", "Sauron es muy malo", -1)
+    director = Director("Peter Jackson", 9)
 
-        pelicula.director = director
+    pelicula.director = director
 
-        assert pelicula.titulo == "El señor de los anillos"
-        assert pelicula.sinopsis == "Sauron es muy malo"
-        assert pelicula._director_id == 9
-        assert pelicula.id == -1
-        assert pelicula.director == director
+    assert pelicula.titulo == "El señor de los anillos"
+    assert pelicula.sinopsis == "Sauron es muy malo"
+    assert pelicula._director_id == 9
+    assert pelicula.id == -1
+    assert pelicula.director == director
 
+def test_dao_peliculas_traer_todos():
+    dao = DAO_CSV_Pelicula("tests/data/peliculas.csv")
+    peliculas = dao.todos()
+
+    assert len(peliculas) == 5
+    assert peliculas[1] == Pelicula("Los siete samuráis", "Una banda de forajidos atemorizan a los habitantes de un pequeño pueblo, saqueándolos periódicamente sin piedad. Para repeler estos ataques, los aldeanos deciden contratar a mercenarios. Finalmente, consiguen los servicios de 7 guerreros, 7 samurais dispuestos a defenderlos a cambio, tan solo, de cobijo y comida.", 2, 17)
+
+def test_dao_peliculas_guardar_pelicula():
+    dao = DAO_CSV_Pelicula("tests/data/peliculas.csv")
+    
+    # Guardar una nueva película
+    nueva_pelicula = Pelicula("Nueva Pelicula", "Sinopsis de prueba", 3, 66)
+    dao.guardar(nueva_pelicula)
+    
+    # Consultar todas las películas y verificar que se ha añadido
+    peliculas = dao.todos()
+    assert len(peliculas) == 6
+    assert peliculas[-1] == nueva_pelicula
+    
+    # Borrar la película recién añadida para restaurar el estado original
+    dao.borrar(66)
+    
+    # Consultar todas las películas y verificar que se ha eliminado
+    peliculas = dao.todos()
+    assert len(peliculas) == 5
+    assert nueva_pelicula not in peliculas
+
+def test_dao_peliculas_consultar():
+    dao = DAO_CSV_Pelicula("tests/data/peliculas.csv")
+    pelicula = dao.consultar(17)
+
+    assert pelicula == Pelicula("Los siete samuráis", "Una banda de forajidos atemorizan a los habitantes de un pequeño pueblo, saqueándolos periódicamente sin piedad. Para repeler estos ataques, los aldeanos deciden contratar a mercenarios. Finalmente, consiguen los servicios de 7 guerreros, 7 samurais dispuestos a defenderlos a cambio, tan solo, de cobijo y comida.", 2, 17)
+
+def test_dao_peliculas_borrar_pelicula():
+    dao = DAO_CSV_Pelicula("tests/data/peliculas.csv")
+    
+    dao.borrar(64)
+    peliculas = dao.todos()
+
+    assert len(peliculas) == 4
+    assert Pelicula("El infierno del odio", "En un momento crucial de su vida financiera, Gondo, un hombre de negocios, recibe la noticia de que su hijo ha sido secuestrado, y el rescate exigido es una cantidad de dinero similar a la que necesita para cerrar una importante negociación. Gondo está dispuesto a pagar el rescate hasta que comprende que los secuestradores se han equivocado y se han llevado al hijo del chófer. Ahora deberá decidir si el dinero es más importante que la vida del niño.", 2, 64) not in peliculas
+
+    dao.guardar(Pelicula("El infierno del odio", "En un momento crucial de su vida financiera, Gondo, un hombre de negocios, recibe la noticia de que su hijo ha sido secuestrado, y el rescate exigido es una cantidad de dinero similar a la que necesita para cerrar una importante negociación. Gondo está dispuesto a pagar el rescate hasta que comprende que los secuestradores se han equivocado y se han llevado al hijo del chófer. Ahora deberá decidir si el dinero es más importante que la vida del niño.", 2, 64))
+
+def test_dao_peliculas_actualizar_pelicula():
+    dao = DAO_CSV_Pelicula("tests/data/peliculas.csv")
+    
+    pelicula = dao.consultar(6)
+    pelicula.titulo = "Un amor contra viento y marea Actualizada"
+    dao.actualizar(pelicula)
+    
+    pelicula_actualizada = dao.consultar(6)
+    assert pelicula_actualizada.titulo == "Un amor contra viento y marea Actualizada"
+
+    pelicula.titulo = "Un amor contra viento y marea"  # Restaurar valor original
+    dao.actualizar(pelicula)
